dddcb3bb84f2b54c9553982a9a9ae6f44079e81f
Геннадий <gemiyur@live.ru>
2025-08-01 19:02:42 +0300

    Изменения в классе Cycle.

--- Crocoteka/Crocoteka/Models/Cycle.cs
+++ Crocoteka/Crocoteka/Models/Cycle.cs
@@ -1,21 +1,19 @@
-﻿using Gemiyur.Comparers;
-
-namespace Crocoteka.Models;
+﻿namespace Crocoteka.Models;
 
 /// <summary>
-/// Класс цикла книг.
+/// Класс серии книг.
 /// </summary>
 public class Cycle : BaseModel
 {
     /// <summary>
-    /// Идентификатор цикла книг.
+    /// Идентификатор серии книг.
     /// </summary>
     public int CycleId { get; set; }
 
     private string title = string.Empty;
 
     /// <summary>
-    /// Название цикла книг.
+    /// Название серии книг.
     /// </summary>
     public string Title
     {
@@ -27,15 +25,18 @@ public class Cycle : BaseModel
         }
     }
 
-    // TODO: Модель данных изменена и этот компаратор для сортировки книг цикла не годится.
+    private string annotation = string.Empty;
 
-    ///// <summary>
-    ///// Компаратор сравнения книг для сортировки книг в цикле книг.
-    ///// </summary>
-    ///// <remarks>
-    ///// Данный компаратор выбран для сортировки по алфавиту внутри номера книги в цикле,
-    ///// если есть одинаковые номера книг, в том числе если номеров книг нет или они не указаны (номер = 0).
-    ///// </remarks>
-    //public static readonly MultiKeyComparer BooksComparer =
-    //    new([new IntKeyComparer(x => ((Book)x).Number), new StringKeyComparer(x => ((Book)x).Title)]);
+    /// <summary>
+    /// Аннотация к серии книг.
+    /// </summary>
+    public string Annotation
+    {
+        get => annotation;
+        set
+        {
+            annotation = value ?? string.Empty;
+            OnPropertyChanged("Annotation");
+        }
+    }
 }
