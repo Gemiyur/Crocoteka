e7993cfee4db2db02e2384f371ee5f8de8da5677
Геннадий <gemiyur@live.ru>
2025-08-03 19:53:57 +0300

    Рефакторинг AboutDialog.

--- Crocoteka/Crocoteka/Dialogs/AboutDialog.xaml.cs
+++ Crocoteka/Crocoteka/Dialogs/AboutDialog.xaml.cs
@@ -1,42 +1,61 @@
 ﻿using System.Reflection;
+using System.Text;
 using System.Windows;
 
-namespace Crocoteka.Dialogs
+namespace Crocoteka.Dialogs;
+
+/// <summary>
+/// Класс окна "О программе".
+/// </summary>
+public partial class AboutDialog : Window
 {
-    /// <summary>
-    /// Класс окна "О программе".
-    /// </summary>
-    public partial class AboutDialog : Window
+    public AboutDialog()
     {
-        public AboutDialog()
+        InitializeComponent();
+
+        var assembly = Assembly.GetExecutingAssembly();
+
+        string version;
+        var array = assembly.GetName().Version?.ToString().Split('.');
+        if (array == null || array.Length == 0)
         {
-            InitializeComponent();
-
-            var assembly = Assembly.GetExecutingAssembly();
-            var array = assembly.GetName().Version.ToString().Split('.');
-            var version = $"Версия: {array[0]}.{array[1]}.{array[2]}";
-            //var company =
-            //    ((AssemblyCompanyAttribute)assembly.GetCustomAttributes(typeof(AssemblyCompanyAttribute), true)
-            //    .First()).Company;
-            var product =
-                ((AssemblyProductAttribute)assembly.GetCustomAttributes(typeof(AssemblyProductAttribute), true)
-                .First()).Product;
-            var description =
-                ((AssemblyDescriptionAttribute)assembly.GetCustomAttributes(typeof(AssemblyDescriptionAttribute), true)
-                .First()).Description.Replace("\r\n", "\n");
-            var copyright =
-                ((AssemblyCopyrightAttribute)assembly.GetCustomAttributes(typeof(AssemblyCopyrightAttribute), true)
-                .First()).Copyright;
-
-            ProductTextBlock.Text = product;
-            DescriptionTextBlock.Text = description;
-            CopyrightTextBlock.Text = copyright;
-            VersionTextBlock.Text = version;
+            version = "Версия не указана";
+        }
+        else
+        {
+            var count = 3;
+            if (array.Length < count)
+                count = array.Length;
+            var sb = new StringBuilder();
+            for (var i = 0; i < count; i++)
+            {
+                sb.Append(array[i]);
+                if (i < count - 1)
+                    sb.Append('.');
+            }
+            version = $"Версия: {sb.ToString()}";
         }
 
-        private void CloseButton_Click(object sender, RoutedEventArgs e)
-        {
-            Close();
-        }
+        var product =
+            ((AssemblyProductAttribute)assembly.GetCustomAttributes(typeof(AssemblyProductAttribute), true)
+            .First()).Product;
+
+        var description =
+            ((AssemblyDescriptionAttribute)assembly.GetCustomAttributes(typeof(AssemblyDescriptionAttribute), true)
+            .First()).Description.Replace("\r\n", "\n");
+
+        var copyright =
+            ((AssemblyCopyrightAttribute)assembly.GetCustomAttributes(typeof(AssemblyCopyrightAttribute), true)
+            .First()).Copyright;
+
+        ProductTextBlock.Text = product;
+        DescriptionTextBlock.Text = description;
+        CopyrightTextBlock.Text = copyright;
+        VersionTextBlock.Text = version;
+    }
+
+    private void CloseButton_Click(object sender, RoutedEventArgs e)
+    {
+        Close();
     }
 }
