c7d31491804aeab39b79941d43f5729008c84d81
Геннадий <gemiyur@live.ru>
2025-08-09 20:50:30 +0300

    Взаимодействие элементов главного окна.

--- Crocoteka/Crocoteka/MainWindow.xaml.cs
+++ Crocoteka/Crocoteka/MainWindow.xaml.cs
@@ -1,14 +1,7 @@
 ﻿using System.IO;
-using System.Text;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using Gemiyur.Collections;
 using Crocoteka.Dialogs;
 using Crocoteka.Models;
@@ -41,6 +34,9 @@ public partial class MainWindow : Window
     /// </summary>
     private readonly ObservableCollectionEx<Genre> Genres = [];
 
+    /// <summary>
+    /// Инициализирует новый экземпляр класса.
+    /// </summary>
     public MainWindow()
     {
         InitializeComponent();
@@ -76,20 +72,170 @@ public partial class MainWindow : Window
     }
 
     /// <summary>
+    /// Выделяет указанную книгу в списке отображаемых книг.
+    /// </summary>
+    /// <param name="book">Книга.</param>
+    /// <remarks>Если указанной книги в списке нет, то ничего не делает.</remarks>
+    private void SelectBookInShownBooks(Book book)
+    {
+        if (ShownBooks.Contains(book))
+        {
+            BooksListBox.SelectedItem = book;
+            BooksListBox.ScrollIntoView(BooksListBox.SelectedItem);
+        }
+    }
+
+    /// <summary>
+    /// Отображает окно информации о книге.
+    /// </summary>
+    /// <param name="book">Книга.</param>
+    public void ShowBookInfo(Book book)
+    {
+        //var window = App.FindBookInfoWindow();
+        //if (window != null)
+        //{
+        //    if (window.Book != book)
+        //        window.Book = book;
+        //    App.RestoreWindow(window);
+        //    window.Activate();
+        //}
+        //else
+        //{
+        //    new BookInfoDialog(book) { Owner = this }.Show();
+        //}
+    }
+
+    /// <summary>
+    /// Обновляет списки панели навигации.
+    /// </summary>
+    /// <param name="authors">Обновить список авторов.</param>
+    /// <param name="cycles">Обновить список серий.</param>
+    /// <param name="genres">Обновить список жанров.</param>
+    private void UpdateNavPanel(bool authors, bool cycles, bool genres)
+    {
+        LockNavHandlers();
+        if (authors)
+        {
+            var selectedAuthor = (Author)AuthorsListBox.SelectedItem;
+            Authors.ReplaceRange(Db.GetAuthors());
+            if (selectedAuthor != null)
+            {
+                AuthorsListBox.SelectedItem = Authors.FirstOrDefault(x => x.AuthorId == selectedAuthor.AuthorId);
+                if (AuthorsListBox.SelectedItem != null)
+                    AuthorsListBox.ScrollIntoView(AuthorsListBox.SelectedItem);
+            }
+        }
+        if (cycles)
+        {
+            var selectedCycle = (Cycle)CyclesListBox.SelectedItem;
+            Cycles.ReplaceRange(Db.GetCycles());
+            if (selectedCycle != null)
+            {
+                CyclesListBox.SelectedItem = Cycles.FirstOrDefault(x => x.CycleId == selectedCycle.CycleId);
+                if (CyclesListBox.SelectedItem != null)
+                    CyclesListBox.ScrollIntoView(CyclesListBox.SelectedItem);
+            }
+        }
+        if (genres)
+        {
+            var selectedTag = (Genre)GenresListBox.SelectedItem;
+            Genres.ReplaceRange(Db.GetGenres());
+            if (selectedTag != null)
+            {
+                GenresListBox.SelectedItem = Genres.FirstOrDefault(x => x.Equals(selectedTag));
+                if (GenresListBox.SelectedItem != null)
+                    GenresListBox.ScrollIntoView(GenresListBox.SelectedItem);
+            }
+        }
+        if (AuthorsListBox.SelectedItem == null &&
+            CyclesListBox.SelectedItem == null &&
+            GenresListBox.SelectedItem == null)
+        {
+            AllBooksToggleButton.IsChecked = true;
+        }
+        UnlockNavHandlers();
+    }
+
+    /// <summary>
+    /// Обновляет список отображаемых книг.
+    /// </summary>
+    private void UpdateShownBooks()
+    {
+        if (AllBooksToggleButton.IsChecked == true)
+        {
+            ShownBooks.ReplaceRange(Library.Books.OrderBy(x => x.Title, StringComparer.CurrentCultureIgnoreCase));
+            BooksListBox.ItemTemplate = (DataTemplate)FindResource("BookDataTemplate");
+        }
+        else if (AuthorsListBox.SelectedItem != null)
+        {
+            var author = (Author)AuthorsListBox.SelectedItem;
+            var books = Library.GetAuthorBooks(author.AuthorId);
+            ShownBooks.ReplaceRange(books);
+            BooksListBox.ItemTemplate = (DataTemplate)FindResource("BookDataTemplate");
+        }
+        else if (CyclesListBox.SelectedItem != null)
+        {
+            var cycle = (Cycle)CyclesListBox.SelectedItem;
+            var books = Library.GetCycleBooks(cycle.CycleId);
+            ShownBooks.ReplaceRange(books);
+            BooksListBox.ItemTemplate = (DataTemplate)FindResource("BookCycleDataTemplate");
+        }
+        else if (GenresListBox.SelectedItem != null)
+        {
+            var tag = (Genre)GenresListBox.SelectedItem;
+            var books = Library.GetGenreBooks(tag.GenreId);
+            ShownBooks.ReplaceRange(books);
+            BooksListBox.ItemTemplate = (DataTemplate)FindResource("BookDataTemplate");
+        }
+        UpdateStatusBarBooksCount();
+    }
+
+    /// <summary>
     /// Обновляет количество отображаемых книг в строке статуса.
     /// </summary>
     private void UpdateStatusBarBooksCount() => BooksCountTextBlock.Text = BooksListBox.Items.Count.ToString();
+
+    #region Блокировка и разблокировка обработчиков событий элементов панели навигации.
+
+    /// <summary>
+    /// Блокируются ли обработчики событий элементов панели навигации.
+    /// </summary>
+    private bool NavHandlersLocked;
+
+    /// <summary>
+    /// Блокирует обработчики событий элементов панели навигации.
+    /// </summary>
+    private void LockNavHandlers() => NavHandlersLocked = true;
+
+    /// <summary>
+    /// Разблокирует обработчики событий элементов панели навигации.
+    /// </summary>
+    private void UnlockNavHandlers() => NavHandlersLocked = false;
+
+    #endregion
 
     #region Обработчики событий окна.
 
     private void Window_Loaded(object sender, RoutedEventArgs e)
     {
-
+        //if (Properties.Settings.Default.SaveMainWindowLocation &&
+        //    App.SizeDefined(Properties.Settings.Default.MainWindowSize))
+        //{
+        //    Left = Properties.Settings.Default.MainWindowPos.X;
+        //    Top = Properties.Settings.Default.MainWindowPos.Y;
+        //    Width = Properties.Settings.Default.MainWindowSize.Width;
+        //    Height = Properties.Settings.Default.MainWindowSize.Height;
+        //}
     }
 
     private void Window_Closed(object sender, EventArgs e)
     {
-
+        //if (Properties.Settings.Default.SaveMainWindowLocation)
+        //{
+        //    Properties.Settings.Default.MainWindowPos = new System.Drawing.Point((int)Left, (int)Top);
+        //    Properties.Settings.Default.MainWindowSize = new System.Drawing.Size((int)Width, (int)Height);
+        //}
+        //Properties.Settings.Default.Save();
     }
 
     #endregion
@@ -98,36 +244,71 @@ public partial class MainWindow : Window
 
     private void AllBooksToggleButton_Click(object sender, RoutedEventArgs e)
     {
-
+        if (AllBooksToggleButton.IsChecked != true)
+        {
+            AllBooksToggleButton.IsChecked = true;
+            return;
+        }
+        LockNavHandlers();
+        AuthorsListBox.SelectedIndex = -1;
+        CyclesListBox.SelectedIndex = -1;
+        GenresListBox.SelectedIndex = -1;
+        UnlockNavHandlers();
+        UpdateShownBooks();
     }
 
     private void AuthorsListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
     {
-
+        if (NavHandlersLocked)
+            return;
+        AllBooksToggleButton.IsChecked = AuthorsListBox.SelectedIndex < 0;
+        LockNavHandlers();
+        CyclesListBox.SelectedIndex = -1;
+        GenresListBox.SelectedIndex = -1;
+        UnlockNavHandlers();
+        UpdateShownBooks();
     }
 
     private void CyclesListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
     {
-
+        if (NavHandlersLocked)
+            return;
+        AllBooksToggleButton.IsChecked = CyclesListBox.SelectedIndex < 0;
+        LockNavHandlers();
+        AuthorsListBox.SelectedIndex = -1;
+        GenresListBox.SelectedIndex = -1;
+        UnlockNavHandlers();
+        UpdateShownBooks();
     }
 
     private void GenresListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
     {
-
+        if (NavHandlersLocked)
+            return;
+        AllBooksToggleButton.IsChecked = GenresListBox.SelectedIndex < 0;
+        LockNavHandlers();
+        AuthorsListBox.SelectedIndex = -1;
+        CyclesListBox.SelectedIndex = -1;
+        UnlockNavHandlers();
+        UpdateShownBooks();
     }
 
     #endregion
 
     #region Обработчики событий элемента списка книг.
 
-    private void BooksListBox_MouseDoubleClick(object sender, MouseButtonEventArgs e)
-    {
-
-    }
-
     private void BooksListBox_ContextMenuOpening(object sender, ContextMenuEventArgs e)
     {
+        if (e.OriginalSource is not TextBlock)
+        {
+            e.Handled = true;
+        }
+    }
 
+    private void BooksListBox_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+    {
+        if (BooksListBox.SelectedItem != null && (e.OriginalSource is TextBlock || e.OriginalSource is Border))
+            ShowBookInfo((Book)BooksListBox.SelectedItem);
     }
 
     #endregion
