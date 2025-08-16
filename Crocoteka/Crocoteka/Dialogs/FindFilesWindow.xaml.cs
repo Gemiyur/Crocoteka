3128e3d1ebbaa37cb2dc36ab54fe4cc158b8cf92
Геннадий <gemiyur@live.ru>
2025-08-16 19:53:21 +0300

    Макет окна поиска файлов.

--- Crocoteka/Crocoteka/Dialogs/FindFilesWindow.xaml.cs
+++ Crocoteka/Crocoteka/Dialogs/FindFilesWindow.xaml.cs
@@ -15,12 +15,47 @@ using System.Windows.Shapes;
 namespace Crocoteka.Dialogs;
 
 /// <summary>
-/// Класс окна найденных файлов книг.
+/// Класс окна поиска файлов книг.
 /// </summary>
 public partial class FindFilesWindow : Window
 {
     public FindFilesWindow()
     {
         InitializeComponent();
+    }
+
+    private void Window_Loaded(object sender, RoutedEventArgs e)
+    {
+
+    }
+
+    private void FolderButton_Click(object sender, RoutedEventArgs e)
+    {
+
+    }
+
+    private void TypeComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+    {
+
+    }
+
+    private void NotInLibraryCheckBox_Click(object sender, RoutedEventArgs e)
+    {
+
+    }
+
+    private void FilesListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+    {
+
+    }
+
+    private void BookButton_Click(object sender, RoutedEventArgs e)
+    {
+
+    }
+
+    private void CloseButton_Click(object sender, RoutedEventArgs e)
+    {
+        Close();
     }
 }
