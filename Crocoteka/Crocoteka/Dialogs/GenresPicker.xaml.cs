84e788a459270c09b2b2e95bf0cbf855d1150a51
Геннадий <gemiyur@live.ru>
2025-09-03 20:55:06 +0300

    Диалог выбора жанров.

---
+++ Crocoteka/Crocoteka/Dialogs/GenresPicker.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using Crocoteka.Models;
+using Crocoteka.Tools;
+
+namespace Crocoteka.Dialogs;
+
+/// <summary>
+/// Класс окна выбора жанров.
+/// </summary>
+public partial class GenresPicker : Window
+{
+    /// <summary>
+    /// Список выбранных жанров.
+    /// </summary>
+    public List<Genre> PickedGenres = [];
+
+    public GenresPicker()
+    {
+        InitializeComponent();
+        GenresListBox.ItemsSource = Library.Genres;
+    }
+
+    private void GenresListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+    {
+        PickButton.IsEnabled = GenresListBox.SelectedIndex >= 0;
+    }
+
+    private void PickButton_Click(object sender, RoutedEventArgs e)
+    {
+        PickedGenres.AddRange(GenresListBox.SelectedItems.Cast<Genre>());
+        DialogResult = true;
+    }
+
+    private void CancelButton_Click(object sender, RoutedEventArgs e) => DialogResult = false;
+}
