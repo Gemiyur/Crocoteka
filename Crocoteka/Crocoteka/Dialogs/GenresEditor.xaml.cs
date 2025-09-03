899fc027fb49c36a171e6031c1961e3864e207ad
Геннадий <gemiyur@live.ru>
2025-09-03 17:16:15 +0300

    Макет редактора жанров.

---
+++ Crocoteka/Crocoteka/Dialogs/GenresEditor.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Crocoteka.Dialogs;
+
+/// <summary>
+/// Класс редактора жанров.
+/// </summary>
+public partial class GenresEditor : Window
+{
+    public GenresEditor()
+    {
+        InitializeComponent();
+    }
+
+    private void GenresListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+    {
+
+    }
+
+    private void AddButton_Click(object sender, RoutedEventArgs e)
+    {
+
+    }
+
+    private void EditButton_Click(object sender, RoutedEventArgs e)
+    {
+
+    }
+
+    private void DeleteButton_Click(object sender, RoutedEventArgs e)
+    {
+
+    }
+
+    private void CloseButton_Click(object sender, RoutedEventArgs e)
+    {
+
+    }
+}
