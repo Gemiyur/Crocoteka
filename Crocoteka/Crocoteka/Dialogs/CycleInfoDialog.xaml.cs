cdb9fcc934b1736ea339fb032782cdf0662554db
Геннадий <gemiyur@live.ru>
2025-10-06 19:35:57 +0300

    Сохранение позиции и размеров окна "О серии".

--- Crocoteka/Crocoteka/Dialogs/CycleInfoDialog.xaml.cs
+++ Crocoteka/Crocoteka/Dialogs/CycleInfoDialog.xaml.cs
@@ -18,12 +18,23 @@ public partial class CycleInfoDialog : Window
 
     private void Window_Loaded(object sender, RoutedEventArgs e)
     {
-
+        if (Properties.Settings.Default.SaveInfoWindowsLocation &&
+            App.SizeDefined(Properties.Settings.Default.CycleInfoWindowSize))
+        {
+            Left = Properties.Settings.Default.CycleInfoWindowPos.X;
+            Top = Properties.Settings.Default.CycleInfoWindowPos.Y;
+            Width = Properties.Settings.Default.CycleInfoWindowSize.Width;
+            Height = Properties.Settings.Default.CycleInfoWindowSize.Height;
+        }
     }
 
     private void Window_Closed(object sender, EventArgs e)
     {
-
+        if (Properties.Settings.Default.SaveInfoWindowsLocation)
+        {
+            Properties.Settings.Default.CycleInfoWindowPos = new System.Drawing.Point((int)Left, (int)Top);
+            Properties.Settings.Default.CycleInfoWindowSize = new System.Drawing.Size((int)Width, (int)Height);
+        }
     }
 
     private void CloseButton_Click(object sender, RoutedEventArgs e) => Close();
