06dcf007ed4de4bcc59a9e23dc9d188443fecf79
Геннадий <gemiyur@live.ru>
2025-10-06 19:29:52 +0300

    Сохранение позиции и размеров окна "Об авторе".

--- Crocoteka/Crocoteka/Dialogs/AuthorInfoDialog.xaml.cs
+++ Crocoteka/Crocoteka/Dialogs/AuthorInfoDialog.xaml.cs
@@ -18,12 +18,23 @@ public partial class AuthorInfoDialog : Window
 
     private void Window_Loaded(object sender, RoutedEventArgs e)
     {
-
+        if (Properties.Settings.Default.SaveInfoWindowsLocation &&
+            App.SizeDefined(Properties.Settings.Default.AuthorInfoWindowSize))
+        {
+            Left = Properties.Settings.Default.AuthorInfoWindowPos.X;
+            Top = Properties.Settings.Default.AuthorInfoWindowPos.Y;
+            Width = Properties.Settings.Default.AuthorInfoWindowSize.Width;
+            Height = Properties.Settings.Default.AuthorInfoWindowSize.Height;
+        }
     }
 
     private void Window_Closed(object sender, EventArgs e)
     {
-
+        if (Properties.Settings.Default.SaveInfoWindowsLocation)
+        {
+            Properties.Settings.Default.AuthorInfoWindowPos = new System.Drawing.Point((int)Left, (int)Top);
+            Properties.Settings.Default.AuthorInfoWindowSize = new System.Drawing.Size((int)Width, (int)Height);
+        }
     }
 
     private void CloseButton_Click(object sender, RoutedEventArgs e) => Close();
