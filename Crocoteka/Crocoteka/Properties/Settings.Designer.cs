0d002d2a4c7212ced536691c36a238f853837b46
Геннадий <gemiyur@live.ru>
2025-10-12 15:46:22 +0300

    Изменил пути к БД по-умолчанию.

--- Crocoteka/Crocoteka/Properties/Settings.Designer.cs
+++ Crocoteka/Crocoteka/Properties/Settings.Designer.cs
@@ -25,7 +25,7 @@ namespace Crocoteka.Properties {
         
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("C:\\Databases\\LiteDB\\Крокотека\\Crocoteka.db")]
+        [global::System.Configuration.DefaultSettingValueAttribute("C:\\Databases\\LiteDB\\Crocoteka\\Crocoteka.db")]
         public string DbName {
             get {
                 return ((string)(this["DbName"]));
@@ -37,7 +37,7 @@ namespace Crocoteka.Properties {
         
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("C:\\Databases\\LiteDB\\Крокотека\\CrocotekaDebug.db")]
+        [global::System.Configuration.DefaultSettingValueAttribute("C:\\Databases\\LiteDB\\Crocoteka\\CrocotekaDebug.db")]
         public string DebugDbName {
             get {
                 return ((string)(this["DebugDbName"]));
