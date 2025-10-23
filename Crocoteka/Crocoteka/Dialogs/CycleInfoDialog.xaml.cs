47c3342fbd94902a32acb7199317e6e07de02c2e
Геннадий <gemiyur@live.ru>
2025-10-23 17:49:04 +0300

    Запрет сворачивания и разворачивания окна "О серии".

--- Crocoteka/Crocoteka/Dialogs/CycleInfoDialog.xaml.cs
+++ Crocoteka/Crocoteka/Dialogs/CycleInfoDialog.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows;
+﻿using System.Runtime.InteropServices;
+using System.Windows;
+using System.Windows.Interop;
 using Crocoteka.Models;
 
 namespace Crocoteka.Dialogs;
@@ -8,12 +10,28 @@ namespace Crocoteka.Dialogs;
 /// </summary>
 public partial class CycleInfoDialog : Window
 {
+    [DllImport("user32.dll")]
+    private static extern int GetWindowLong(IntPtr hWnd, int nIndex);
+    [DllImport("user32.dll")]
+    private static extern int SetWindowLong(IntPtr hWnd, int nIndex, int dwNewLong);
+
+    private const int GWL_STYLE = -16;
+    private const int WS_MAXIMIZEBOX = 0x10000;
+    private const int WS_MINIMIZEBOX = 0x20000;
+
     public CycleInfoDialog(Cycle cycle)
     {
         InitializeComponent();
         TitleTextBlock.FontSize = FontSize + 2;
         TitleTextBlock.Text = cycle.Title;
         AnnotationTextBox.Text = cycle.Annotation;
+    }
+
+    private void Window_SourceInitialized(object sender, EventArgs e)
+    {
+        IntPtr handle = new WindowInteropHelper(this).Handle;
+        _ = SetWindowLong(handle, GWL_STYLE, GetWindowLong(handle, GWL_STYLE) & ~WS_MINIMIZEBOX);
+        _ = SetWindowLong(handle, GWL_STYLE, GetWindowLong(handle, GWL_STYLE) & ~WS_MAXIMIZEBOX);
     }
 
     private void Window_Loaded(object sender, RoutedEventArgs e)
