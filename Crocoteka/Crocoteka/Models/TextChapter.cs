13ede56ec244a23bd706e810704861e76ba06aee
Геннадий <gemiyur@live.ru>
2026-02-02 20:55:30 +0300

    Классы текстовой книги.

---
+++ Crocoteka/Crocoteka/Models/TextChapter.cs
@@ -0,0 +1,37 @@
+﻿namespace Crocoteka.Models;
+
+/// <summary>
+/// Класс раздела текстовой книги.
+/// </summary>
+public class TextChapter : BaseModel
+{
+    private int position;
+
+    /// <summary>
+    /// Позиция раздела текстовой книги.
+    /// </summary>
+    public int Position
+    {
+        get => position;
+        set
+        {
+            position = value;
+            OnPropertyChanged("Position");
+        }
+    }
+
+    private string title = string.Empty;
+
+    /// <summary>
+    /// Название раздела текстовой книги.
+    /// </summary>
+    public string Title
+    {
+        get => title;
+        set
+        {
+            title = value ?? string.Empty;
+            OnPropertyChanged("Title");
+        }
+    }
+}
